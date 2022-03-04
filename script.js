3609522ee6ae938701ace56253b1f3a2b6c5d176
ejiritakeshi <98149167+ejiritakeshi@users.noreply.github.com>
2022-03-04 14:14:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,50 +2,64 @@
 // 1行目に記載している 'use strict' は削除しないでください
 
 const letters = ["アイウエオ",
-                 "カキクケコ",
-                 "サシスセソ",
-                 "タチツテト",
-                 "ナニヌネノ",
-                 "ハヒフヘホ",
-                 "マミムメモ",
-                 "ヤユヨ",
-                 "ラリルレロ",
-                 "ワンー",
-                 "ァィゥェォ",
-                 "ガギグゲゴ",
-                 "ザジズゼゾ",
-                 "ダヂヅデド",
-                 "ッ",
-                 "バビブベボ",
-                 "パピプペポ",
-                 "ャュョ"];
+  "カキクケコ",
+  "サシスセソ",
+  "タチツテト",
+  "ナニヌネノ",
+  "ハヒフヘホ",
+  "マミムメモ",
+  "ヤユヨ",
+  "ラリルレロ",
+  "ワンー",
+  "ァィゥェォ",
+  "ガギグゲゴ",
+  "ザジズゼゾ",
+  "ダヂヅデド",
+  "ッ",
+  "バビブベボ",
+  "パピプペポ",
+  "ャュョ"];
 
 const base = document.querySelector('.keybase');
 const keys = [];
 for (const letter of letters) {
-    const div = document.createElement('div');
-    div.className = 'column';
-    base.appendChild(div);
-    for (const char of letter) {
-        const span = document.createElement('span');
-        span.textContent = char;
-        span.id = char;
-        span.addEventListener('click', clickLetter);
-        div.appendChild(span);
-        keys.push(span);
-    }
+  const div = document.createElement('div');
+  div.className = 'column';
+  base.appendChild(div);
+  for (const char of letter) {
+    const span = document.createElement('span');
+    span.textContent = char;
+    span.id = char;
+    span.addEventListener('click', clickLetter);
+    div.appendChild(span);
+    keys.push(span);
+  }
 }
 
 const answerBase = document.querySelector(".answerbase");
 for (let j = 0; j < 10; j++) {
-    const answerRow = document.createElement('div');
-    answerRow.className = 'answerRow';
-    answerBase.appendChild(answerRow);
-    for (let i = 0; i < 5; i++) {
-        const span = document.createElement('span');
-        answerRow.appendChild(span);
-    }
+  const answerRow = document.createElement('div');
+  answerRow.className = 'answerRow';
+  answerBase.appendChild(answerRow);
+  for (let i = 0; i < 5; i++) {
+    const span = document.createElement('span');
+    answerRow.appendChild(span);
+  }
 }
+
+let hints = allPokemon;
+function displayHints() {
+  const hintBase = document.querySelector('.hintbase');
+  while (hintBase.firstChild) {
+    hintBase.removeChild(hintBase.firstChild);
+  }
+  hints.forEach(pokemon => {
+    const pokeElement = document.createElement('p');
+    pokeElement.textContent = pokemon;
+    hintBase.appendChild(pokeElement);
+  });
+}
+displayHints();
 
 const deleteButton = document.querySelector('.delete');
 deleteButton.addEventListener('click', deleteLast);
@@ -65,7 +79,6 @@ let answerWord = "";
 const answerRows = document.querySelectorAll('.answerRow');
 
 function clickLetter() {
-  console.log('click');
   if (answerWord.length > 4) {
     return;
   }
@@ -74,7 +87,6 @@ function clickLetter() {
 }
 
 function deleteLast() {
-  console.log('delete');
   if (answerWord.length === 0) {
     return;
   }
@@ -90,72 +102,78 @@ function displayAnswer() {
 }
 
 function checkTheAnswer() {
-  console.log('check');
   if (answerWord.length < 5) {
     return;
   }
-  
+
   const answerRow = answerRows[times].children;
   let correct = correctAnswer;
   for (let i = 0; i < 5; i++) {
-      if (correct[i] === answerWord[i]) {
-          answerRow[i].className = "correct";
-          document.getElementById(correct[i]).className = "correct";
-          correct = correct.slice(0, i) + "?" + correct.slice(i + 1, correct.length);
-      }
+    if (correct[i] === answerWord[i]) {
+      answerRow[i].className = "correct";
+      document.getElementById(correct[i]).className = "correct";
+      correct = correct.slice(0, i) + "?" + correct.slice(i + 1, correct.length);
+      hints = hints.filter( pokemonName => pokemonName[i] === answerWord[i] );
+    }
   }
   if (answerWord === correctAnswer) {
-      // 正解画面を出す。
-      messageBase.style.display = 'flex';
-      message.textContent = "🎊せいかい🎊";
+    // 正解画面を出す。
+    messageBase.style.display = 'flex';
+    message.textContent = "🎉せいかい🎉";
   }
+
   for (let i = 0; i < 5; i++) {
-      let included = false
-      const key = document.getElementById(answerWord[i]);
-      for (let j = 0; j < 5; j++) {
-          if (included) {
-             // 何もしない
-          } else if (answerWord[i] === correct[j]) {
-              answerRow[i].className = "included";
-              if (key.className !== "correct") {
-                  key.className = "included";
-              }
-              correct = correct.slice(0, j) + "?" + correct.slice(j + 1, correct.length);
-              included = true;
-          }
+    let included = false
+    const key = document.getElementById(answerWord[i]);
+    for (let j = 0; j < 5; j++) {
+      if (!included && answerWord[i] === correct[j]) {
+        if (answerRow[i].className !== "correct") {
+          answerRow[i].className = "included";
+          correct = correct.slice(0, j) + "?" + correct.slice(j + 1, correct.length);
+          hints = hints.filter( pokemonName => pokemonName.includes(answerWord[i]) );
+        }
+        if (key.className !== "correct") {
+          key.className = "included";
+        }
+        included = true;
       }
-      if (!included && answerRow[i].className !== "correct") {
-          answerRow[i].className = "incorrect";
-          if (key.className !== "correct") {
-              key.className = "incorrect";
-          }
+    }
+    
+    if (!included && answerRow[i].className !== "correct") {
+      answerRow[i].className = "incorrect";
+      if (key.className !== "correct" && key.className !== "included") {
+        key.className = "incorrect";
+        hints = hints.filter( pokemonName => !pokemonName.includes(answerWord[i]) );
       }
+    }
   }
   answerWord = "";
   times++;
-    
-    if (times >= 10) {
-        // ざんねん
-        messageBase.style.display = 'flex';
-        message.innerHTML = `ざんねん😢<br><br>せいかいは<br>${correctAnswer}<br>でした`;
-    }
+
+  if (times >= 10) {
+    // ざんねん
+    messageBase.style.display = 'flex';
+    message.innerHTML = `ざんねん😢<br><br>せいかいは<br>${correctAnswer}<br>でした`;
+  }
+
+  displayHints();
 }
 
 function next() {
-    messageBase.style.display = 'none';
-    times = 0;
-    answerWord = "";
-    correctAnswer = allPokemon[Math.floor(Math.random() * allPokemon.length)];
-    for (const row of answerRows) {
-        for (const span of row.children) {
-            span.textContent = "";
-            span.classList.remove(...span.classList);
-        }
+  messageBase.style.display = 'none';
+  times = 0;
+  answerWord = "";
+  correctAnswer = allPokemon[Math.floor(Math.random() * allPokemon.length)];
+  hints = allPokemon;
+  for (const row of answerRows) {
+    for (const span of row.children) {
+      span.textContent = "";
+      span.classList.remove(...span.classList);
     }
-    for (const key of keys) {
-        key.classList.remove(...key.classList);
-    }
-    console.log(correctAnswer);
-}
+  }
+  for (const key of keys) {
+    key.classList.remove(...key.classList);
+  }
 
-console.log(correctAnswer);
+  displayHints();
+}
